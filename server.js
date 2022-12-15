4a66753970339b838e98aa12012edcc33e00f491
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-12-15 18:56:02 -0300

    return values with string builder and array functions

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const app = express();
 
 app.get("/", async (request, response) => {
   const allAnimals = await db.collection("animals").find().toArray();
-  console.log(allAnimals);
-  response.send("Welcome to the homepage");
+  response.send(
+    `<h1>Welcome to the page</h1>
+      ${allAnimals.map(animal => `
+      <p>${animal.name} - ${animal.species}</p>`
+    ).join('')}`
+  );
 });
 
 app.get("/admin", (request, response) => {
