b0e12586d0489fa2040848e6671718908a170098
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-12-15 16:11:05 -0300

    setting inital routes to test the webserver

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Hello from node ');
+const express = require('express');
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Welcome to the homepage");
+});
+
+app.get("/admin", (request, response) => {
+  response.send("This is the top secret admin page!");
+});
+
+app.listen(3000);
