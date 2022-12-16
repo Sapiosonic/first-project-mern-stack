684a7807a94e2b397f59d871e5bf70d5341696ec
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-12-15 22:41:52 -0300

    Create App and AnimalCard components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-alert("Testing if webpack is working");
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>Hey, this is from React.</p>
+    </div>
+  );
+}
+
+function AnimalCard(props){
+  return <p>Hi, my name is {props.name} and I am a {props.species}</p>
+}
+
+const root = createRoot(document.querySelector("#app"));
+root.render(<App />);
