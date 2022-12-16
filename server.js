22de75e4b6dd38a3809eca4d37e1cd4cab8b147a
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-12-15 23:07:21 -0300

    Add Axios to the project

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get("/admin", (request, response) => {
   response.render("admin");
 });
 
+app.get("/api/animals", async (request, response) => {
+  const allAnimals = await db.collection("animals").find().toArray();
+  response.json(allAnimals);
+});
+
 async function start() {
   const client = new MongoClient("mongodb://root:root@localhost:27017/AmazingMernApp?&authSource=admin");
   await client.connect();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
+import Axios from "axios";
 
 function App() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    async function go(){
+      const response = await Axios.get("/api/animals");
+      setAnimals(response.data);
+    }
+    go();
+  }, []);
+
   return (
     <div>
       <h1>Hello</h1>
       <p>Hey, this is from React.</p>
+      {animals.map(function (animal){
+        return <AnimalCard name={animal.name} species={animal.species}/>
+      })}
     </div>
   );
 }
